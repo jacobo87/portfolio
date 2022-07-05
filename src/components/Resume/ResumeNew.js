9124f0268a1dd09745d64392270e18c6708f6b57
Jacobo Azmani <azmanijacobo@gmail.com>
2022-07-05 09:58:44 +0200

    Remove Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Resume_Michael_Beebower.pdf";
+import pdf from "../../Assets/cv-Jacobo-Azmani-En.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/beebus/portfolio/master/src/Assets/Resume_Michael_Beebower.pdf";
+  "https://github.com/jacobo87/portfolio/blob/main/src/Assets/cv-Jacobo-Azmani-En.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
